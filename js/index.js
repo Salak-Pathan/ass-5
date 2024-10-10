87e82c2618dc46a7901fe9278adab4bab20fad6e
Salak Pathan <salakpathan007@gmail.com>
2024-10-11 05:43:48 +0600

    logic updated

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ calculateButton.addEventListener('click', function () {
     
     const donate1 = parseFloat(document.getElementById("noakhali").value);
     console.log(donate1);
+    const totalDonate = donate1
+    const totalDonateElement = document.getElementById("total");
+    totalDonateElement.innerText = totalDonate.toFixed(2);
+
+
 });
 
 
@@ -11,6 +16,9 @@ calculateButton2.addEventListener('click', function () {
     
     const donate2 = parseFloat(document.getElementById("feni").value);
     console.log(donate2);
+    const totalDonate = donate2
+    const totalDonateElement = document.getElementById("total");
+    totalDonateElement.innerText = totalDonate.toFixed(2);
 });
 
 const calculateButton3 = document.getElementById("quotabtn");
@@ -18,12 +26,7 @@ calculateButton3.addEventListener('click', function () {
     
     const donate3 = parseFloat(document.getElementById("quota").value);
     console.log(donate3);
-    
-});
-const calculateButton4 = document.getElementById("total");
-calculateButton4.addEventListener('click' , function(){
-    const total = parseFloat(document.getElementById("total").value);
-    const totalDonate = donate1 + donate2 + donate3;
+    const totalDonate = donate3
     const totalDonateElement = document.getElementById("total");
-    totalDonateElement.innerText = totalDonate;
-})
+    totalDonateElement.innerText = totalDonate.toFixed(2);
+});
